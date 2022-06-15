9088cbd7099fbd372f4e57e5d52c5c007f2b8629
Ales Novotny <hi@alesnovotny.cz>
2022-06-16 00:23:28 +0200

    cheer with fetch and promises

--- cheer.ts
+++ cheer.ts
@@ -1,3 +1,11 @@
 function cheer(parametr: string): string {
   return `${parametr} je cyp! Ostrava!!!`;
 }
+
+fetch("http://www.qda.cz").then((resp: Response) => {
+  resp.blob().then((value: Blob) => {
+    value.text().then((str: string) => {
+      console.log(cheer(str));
+    });
+  });
+});
