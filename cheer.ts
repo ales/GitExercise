238cffdff941f1a390c35bba87f64daac315fe80
Ales Novotny <hi@alesnovotny.cz>
2022-06-15 22:52:14 +0200

    privateFunction n tuning

--- cheer.ts
+++ cheer.ts
@@ -6,7 +6,7 @@ type InternalMemory = number; // just for the sake of science
 
 interface Pony {
   run(): Pony;
-  play(s: string): void;
+  play(song: string): void;
   publicProperty: string; // if this wasn't pressent, ponyFactory wouldn't compile for not adhering to interface == return type
 }
 
@@ -14,14 +14,19 @@ function ponyFactory(emojiFromParam: string = "📢"): Pony {
   const privateConst = 42;
   let privateProperty: InternalMemory | null = 0; // nullable -- is this actually the JS way?
 
+  const _setEngines = () => {
+    // privateProperty++; object is possibly null error!
+    privateProperty!++; // bang! it's not!
+    if (privateProperty) privateProperty++; // but this compiles too, what a smart compiler!
+  };
+
   return {
     // object structure starts here { <---
     // i wanted the previous comment to be at the same line as the "return {" but the prettier says no :(
     run(): Pony {
-      // privateProperty++; object is possibly null error!
-      privateProperty!++; // bang! it's not!
+      _setEngines(); // dunno what's the correct convention for private functions naming
       console.log("Run!");
-      return this;
+      return this; // context is king!
     },
     play(song: string): void {
       console.log(
@@ -29,7 +34,7 @@ function ponyFactory(emojiFromParam: string = "📢"): Pony {
       );
     },
     publicProperty: emojiFromParam, // no way to write the type explicitly?
-  };
+  }; // object structure ends here
 }
 
 ponyFactory().run().play("Fortunate Son – Creedence Clearwater Revival");
